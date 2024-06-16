3915cefd7cc1d50134da0a1670bee0e0d7558bb2
MauryIV <mauryhughesiv@gmail.com>
2024-06-15 21:34:20 -0600

    09-vowel-count completed

--- 09-vowel-count/vowel-count.js
+++ 09-vowel-count/vowel-count.js
@@ -1,3 +1,18 @@
 // Write code to return the the number of vowels in `str`
 
-var vowelCount = function(str) {};
+var vowelCount = function(str) {
+  let vowels = 0;
+  const vowelArr = ["a", "e", "i", "o", "u"];
+  for (i=0; i<str.length; i++) {
+    let letter = str[i].toLowerCase();
+    let vowel = '';
+    for (j=0; j<vowelArr.length; j++) {
+      vowel = vowelArr[j];
+      if (letter === vowel) {
+        vowels++;
+      }
+    }
+  }
+  console.log(vowels)
+  return vowels;
+};
