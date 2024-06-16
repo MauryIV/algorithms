a554f4dc70140461ffe628da86a28ca931aaf21e
MauryIV <mauryhughesiv@gmail.com>
2024-06-15 21:09:24 -0600

    08-max-num completed

--- 08-max-num/max-num.js
+++ 08-max-num/max-num.js
@@ -1,3 +1,12 @@
 // Write code to return the largest number in the given array
 
-var maxNum = function(arr) {};
+var maxNum = function(arr) {
+  let largestNum = 0
+  for (i=0; i<arr.length; i++) {
+    let num = arr[i];
+    if (num > largestNum) {
+      largestNum = num
+    }
+  }
+  return largestNum
+};
