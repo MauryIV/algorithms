0e3546cabd70fe7de9e0d31b87f64422b8c5b0a4
MauryIV <mauryhughesiv@gmail.com>
2024-06-16 21:19:08 -0600

    11-factorial completed

--- 11-factorial/factorial.js
+++ 11-factorial/factorial.js
@@ -1,3 +1,9 @@
 // Write code to create a function that returns the factorial of `num`
 
-var factorial = function(num) {};
+var factorial = function(num) {
+  let sum = 1;
+  for (i=1; i<num+1; i++) {
+    sum *= i;
+  }
+  return sum;
+};
