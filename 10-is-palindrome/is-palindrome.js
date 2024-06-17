10566b779f1d3512aaf56a4bb19d90e3f870f3ff
MauryIV <mauryhughesiv@gmail.com>
2024-06-16 20:51:52 -0600

    10-is-palindrome

--- 10-is-palindrome/is-palindrome.js
+++ 10-is-palindrome/is-palindrome.js
@@ -1,3 +1,17 @@
 // Write code that returns true if `str` is a palindrome, and false if `str` is not a palindrome
 
-var isPalindrome = function(str) {};
+var isPalindrome = function(str) {
+  let forwardArr = [];
+  let backwardArr = [];
+  for (i=0; i<str.length; i++) {
+    forwardArr.push(str[i]);
+  }
+  for (j=str.length-1; j>=0; j--) {
+    backwardArr.push(str[j]);
+  }
+  if (forwardArr.join('') === backwardArr.join('')) {
+    return true;
+  } else {
+    return false;
+  }
+};
