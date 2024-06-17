94c38d6c1a56b8767a0dd9b291cca4f714b0ffa0
MauryIV <mauryhughesiv@gmail.com>
2024-06-16 21:42:55 -0600

    12-title-case completed

--- 12-title-case/title-case.js
+++ 12-title-case/title-case.js
@@ -1,3 +1,14 @@
 // Write code to create a function takes a string and returns a new string with the first letters of each word capitalized
 
-var titleCase = function(str) {};
+var titleCase = function(str) {
+  let strArr = [];
+  strArr.push(str[0].toUpperCase());
+  for (i=0; i<str.length; i++) {
+    if (str[i] === ' ') {
+      strArr.push(str[i+1].toUpperCase());
+    } else {
+    strArr.push(str[i+1]);
+    }
+  }
+  return strArr.join('');
+};
