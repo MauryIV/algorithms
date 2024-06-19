db0da49f09faabcf0545bb0793574ca8c0d9d0f7
MauryIV <mauryhughesiv@gmail.com>
2024-06-18 21:28:01 -0600

    14-product-of-largest-two completed

--- 14-product-of-largest-two/product-of-largest-two.js
+++ 14-product-of-largest-two/product-of-largest-two.js
@@ -1,3 +1,15 @@
 // Write code to create a function that accepts an array of numbers, finds the largest two numbers, and returns the product of the two
 
-var productOfLargestTwo = function(arr) {};
+var productOfLargestTwo = function(arr) {
+  let first = -Infinity;
+  let second = -Infinity;
+  for (i=0; i<arr.length; i++) {
+    if (arr[i] > first) {
+      second = first;
+      first = arr[i];
+    } else if (arr[i] > second) {
+      second = arr[i];
+    }
+  }
+  return first * second;
+};
