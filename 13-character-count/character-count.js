aec2841e07bc5d759b4d68e161a649f2be02874e
MauryIV <mauryhughesiv@gmail.com>
2024-06-18 20:10:28 -0600

    13-character-count completed

--- 13-character-count/character-count.js
+++ 13-character-count/character-count.js
@@ -1,3 +1,19 @@
 // Write code to create a function that accepts a string and returns an object containing the number of times each character appears in the string
 
-var characterCount = function(str) {};
+var characterCount = function(str) {
+  let charList = {}
+  for (i=0; i<str.length; i++) {
+    let char = str[i]
+    // if (char === ' ') {
+    //   continue
+    // } (test included spaces)
+    if (charList[char]) {
+      charList[char]++
+    }
+    else {
+      charList[char] = 1
+    }
+  }
+  console.log(charList)
+  return charList
+};
