6a868ebec7bbc9046f6528bb3b04398aa1ddca22
MauryIV <mauryhughesiv@gmail.com>
2024-06-19 18:44:54 -0600

    15-camel-case completed

--- 15-camel-case/camel-case.js
+++ 15-camel-case/camel-case.js
@@ -1,3 +1,16 @@
 // Write code to create a function that accepts a string and returns the string in camelCase
 
-var camelCase = function(str) {};
+var camelCase = function(str) {
+  const camelStr = [];
+  const lowStr = str.toLowerCase();
+  for (i=0; i<lowStr.length; i++) {
+    if (lowStr[i] === ' ') {
+      const cap = (lowStr[i+1]).toUpperCase();
+      camelStr.push(cap);
+      i++;
+    } else {
+      camelStr.push(lowStr[i]);
+    }
+  }
+  return camelStr.join('');
+};
