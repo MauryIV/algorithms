d6ab8a90d14fb4b53602d9376f8a248d8d2ae2f4
MauryIV <mauryhughesiv@gmail.com>
2024-06-20 20:17:24 -0600

    17-is-unique completed

--- 17-is-unique/is-unique.js
+++ 17-is-unique/is-unique.js
@@ -1,4 +1,14 @@
 // Write code to create a function that accepts an array numbers
 // Return `true` is no number appears in the array more than once, else return `false`
 
-var isUnique = function(arr) {};
+var isUnique = function(arr) {
+  let newArr = [];
+  for (i=0; i<arr.length; i++) {
+    if (newArr.includes(arr[i])) {
+      return false;
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+  return true;
+};
