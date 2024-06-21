693cc04b711db4e090b0bf476b64ce58438642e5
MauryIV <mauryhughesiv@gmail.com>
2024-06-21 16:44:11 -0600

    18-is-anagram completed

--- 18-is-anagram/is-anagram.js
+++ 18-is-anagram/is-anagram.js
@@ -1,4 +1,21 @@
 // Write code to create a function that accepts two strings
 // Return true if the strings are anagrams of each other, otherwise return false
 
-var isAnagram = function(strA, strB) {};
+var isAnagram = function(strA, strB) {
+  if (strA.length !== strB.length) {
+    return false;
+  }
+  for (i=0; i<strA.length; i++) {
+    const letterA = strA[i];
+    if (!strB.includes(letterA)) {
+      return false;
+    }
+  }
+  for (i=0; i<strB.length; i++) {
+    const letterB = strB[i];
+    if (!strA.includes(letterB)) {
+      return false;
+    }
+  }
+  return true;
+};
