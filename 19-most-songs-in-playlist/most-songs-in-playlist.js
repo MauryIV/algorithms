1941b9eb35ec524de1ece2f57eeefc66e0135784
MauryIV <mauryhughesiv@gmail.com>
2024-06-30 19:52:53 -0600

    19-most-songs-in-playlist completed

--- 19-most-songs-in-playlist/most-songs-in-playlist.js
+++ 19-most-songs-in-playlist/most-songs-in-playlist.js
@@ -3,5 +3,14 @@
 // Return the maximum number of songs from the list that can fit in a 60 minute playlist
 
 var mostSongsInPlaylist = function(arr) {
-  
+  const sorted = arr.sort();
+  let length = 0;
+  let playlist = 0;
+  for (i=0; i<sorted.length; i++) {
+    if (length + sorted[i] < 60) {
+      length += sorted[i];
+      playlist++;
+    }
+  }
+  return playlist;
 };
