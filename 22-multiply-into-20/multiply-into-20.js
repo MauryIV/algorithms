dc733a4ab09b6f019546a0b3754733de820940d6
MauryIV <mauryhughesiv@gmail.com>
2024-07-02 12:17:53 -0600

    22-multiple-into-20 completed

--- 22-multiply-into-20/multiply-into-20.js
+++ 22-multiply-into-20/multiply-into-20.js
@@ -1,3 +1,11 @@
+// In this activity, you will be writing code to create a function that takes an array of unique numbers and returns true if the product of any two numbers is 20; else returns false.
 const multiplyInto20 = function(arr) {
-  // TODO: Write Your Code Here
+  for (let i=0; i<arr.length; i++) {
+    for (let j=0; j<arr.length; j++) {
+      if (arr[i] * arr[j] === 20) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
