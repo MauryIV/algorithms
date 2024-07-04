33f92dd3509314026f6a7a3a897c7123e0d76c7f
MauryIV <mauryhughesiv@gmail.com>
2024-07-03 21:54:08 -0600

    24-merge-sorted completed

--- 24-merge-sorted/merge-sorted.js
+++ 24-merge-sorted/merge-sorted.js
@@ -1,3 +1,10 @@
 // Write code to merge two sorted arrays into a new sorted array
 
-var mergeSorted = function (arr1, arr2) {};
+var mergeSorted = function (arr1, arr2) {
+  for (i=0; i<arr2.length; i++) {
+    arr1.push(arr2[i]);
+  }
+  arr1.sort((a, b) => a - b);
+  console.log(arr1)
+  return arr1;
+};
