2d230be34a1250b549833663245c2a51793962f0
MauryIV <mauryhughesiv@gmail.com>
2024-07-07 20:03:11 -0600

    29-permutation-substring completed

--- 29-permutation-substring/permutation-substring.js
+++ 29-permutation-substring/permutation-substring.js
@@ -2,4 +2,16 @@
 // Return true if the second string is a substring of any permutation of the first string
 // Else return false
 
-var permutationSubstring = function(str, sub) {};
+var permutationSubstring = function(str, sub) {
+  let copySub = [];
+  for (let i=0; i<sub.length; i++) {
+    if (str.includes(sub[i])) {
+      copySub.push(sub[i]);
+    }
+  }
+  if (sub === copySub.join('')) {
+    return true;
+  } else {
+    return false;
+  }
+};
