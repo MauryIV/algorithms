574b506b067d63f318f480305286dc1ad6460053
MauryIV <mauryhughesiv@gmail.com>
2024-07-07 19:33:42 -0600

    26-array-intersection completed

--- 26-array-intersection/array-intersection.js
+++ 26-array-intersection/array-intersection.js
@@ -1,4 +1,12 @@
 // Write code to create a function that accepts two arrays of numbers
 // Return a new array containing the intersecting elements of the arrays
 
-var arrayIntersection = function(arr1, arr2) {};
+var arrayIntersection = function(arr1, arr2) {
+  let arrIntersect = [];
+  for (let i=0; i<arr1.length; i++) {
+    if (arr2.includes(arr1[i])) {
+      arrIntersect.push(arr1[i]);
+    }
+  }
+  return arrIntersect;
+};
