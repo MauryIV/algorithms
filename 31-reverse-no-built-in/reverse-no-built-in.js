62dc84e9f5831e61559fbdeed39f197e3c603bcb
MauryIV <mauryhughesiv@gmail.com>
2024-07-07 20:49:50 -0600

    31-reverse-no-built-in completed

--- 31-reverse-no-built-in/reverse-no-built-in.js
+++ 31-reverse-no-built-in/reverse-no-built-in.js
@@ -1,4 +1,10 @@
 // Write code to return a new string with all of the characters in `str` reversed.
 // You may NOT use the built-in reverse method
 
-const reverse = (str) => {};
+const reverse = (str) => {
+  let reverseStr = [];
+  for (let i=str.length-1; i>=0; i--) {
+    reverseStr.push(str[i]);
+  }
+  return reverseStr.join('');
+};
