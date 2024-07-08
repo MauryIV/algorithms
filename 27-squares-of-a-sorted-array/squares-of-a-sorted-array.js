5e85cefe3462a1de3d51e9447ee7c3883d66a07a
MauryIV <mauryhughesiv@gmail.com>
2024-07-07 19:40:32 -0600

    27-squares-of-a-sorted-array completed

--- 27-squares-of-a-sorted-array/squares-of-a-sorted-array.js
+++ 27-squares-of-a-sorted-array/squares-of-a-sorted-array.js
@@ -1,3 +1,9 @@
 // Write code to create a function that accepts an array of integers sorted in ascending (increasing) order and returns a new array containing the squares of each number in ascending order
 
-var sortedSquares = function(arr) {};
+var sortedSquares = function(arr) {
+  let arrSquares = [];
+  for (let i=0; i<arr.length; i++) {
+    arrSquares.push(arr[i] * arr[i]);
+  }
+  return arrSquares.sort((a, b) => a - b);
+};
