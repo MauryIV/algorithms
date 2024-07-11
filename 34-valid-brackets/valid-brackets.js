f3140521300df41bda57365f4c2f1b979a75921a
MauryIV <mauryhughesiv@gmail.com>
2024-07-10 20:23:48 -0600

    34-valid-brackets completed

--- 34-valid-brackets/valid-brackets.js
+++ 34-valid-brackets/valid-brackets.js
@@ -1,3 +1,23 @@
 // Write a function to take a string and return true if it contains valid sets of matching brackets, else return false
 
-const validBrackets = function(str) {};
+const validBrackets = function(str) {
+  let tempArr = [];
+  const leftBrackets = ["(", "{", "["];
+  const rightBrackets = [")", "}", "]"];
+  for (let i=0; i<str.length; i++) {
+    if (leftBrackets.includes(str[i])) {
+      tempArr.push(str[i]);
+    } else if ((tempArr[tempArr.length - 1] === leftBrackets[0]) && (str[i] === rightBrackets[0])) {
+      tempArr.pop();
+    } else if ((tempArr[tempArr.length - 1] === leftBrackets[1]) && (str[i] === rightBrackets[1])) {
+      tempArr.pop();
+    } else if ((tempArr[tempArr.length - 1] === leftBrackets[2]) && (str[i] === rightBrackets[2])) {
+      tempArr.pop();
+    }
+  }
+  if (tempArr.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
