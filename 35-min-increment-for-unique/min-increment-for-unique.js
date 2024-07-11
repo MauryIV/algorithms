dc380f6741c74d4eb2f732b4013f9f581451db60
MauryIV <mauryhughesiv@gmail.com>
2024-07-10 21:04:22 -0600

    35-min-increment-for-unique completed

--- 35-min-increment-for-unique/min-increment-for-unique.js
+++ 35-min-increment-for-unique/min-increment-for-unique.js
@@ -1,5 +1,17 @@
 // Write a function to take an array of integers and return the minimum number of increments required across the array to ensure that every number is unique
 
 const minIncrement = function(nums) {
-  
+  let increments = 0;
+  let tempArr = [];
+  let sortedArr = nums.sort((a, b) => a - b);
+  for (let i=0; i<sortedArr.length; i++) {
+    for (let j=0; j<tempArr.length; j++) {
+      if (sortedArr[i] === tempArr[j]) {
+        sortedArr[i] = sortedArr[i] + 1;
+        increments++;
+      }
+    }
+    tempArr.push(sortedArr[i]);
+  }
+  return increments;
 };
