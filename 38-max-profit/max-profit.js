670aeb7130210afebdf3ad96618d3622d220d85b
MauryIV <mauryhughesiv@gmail.com>
2024-07-10 21:41:59 -0600

    38-max-profit completed

--- 38-max-profit/max-profit.js
+++ 38-max-profit/max-profit.js
@@ -3,4 +3,16 @@
 
 // TODO: Complete the function below:
 
-var maxProfit = function (prices) {};
+var maxProfit = function (prices) {
+  let low = Infinity;
+  let high = 0;
+  for (let i=0; i<prices.length; i++) {
+    if (prices[i] > high) {
+      high = prices[i];
+    } if (prices[i] < low) {
+      low = prices[i];
+      high = prices[i];
+    }
+  }
+  return high - low;
+};
