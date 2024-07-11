e9d31ff74f0ef5dcf888aab20fac41aee01f95d6
MauryIV <mauryhughesiv@gmail.com>
2024-07-10 21:08:08 -0600

    36-peak-finder completed

--- 36-peak-finder/peak-finder.js
+++ 36-peak-finder/peak-finder.js
@@ -1,4 +1,12 @@
-const peakFinder = function(nums) {
   // TODO: Write a function that takes an array of integers containing exactly one peak.
   // A peak is defined as a location in the array where the value is greater than every number to the left and every number to the right. Return the value found at the array's peak
+
+const peakFinder = function(nums) {
+  let peak = -Infinity;
+  for (let i=0; i<nums.length; i++) {
+    if (nums[i] > peak) {
+      peak = nums[i];
+    }
+  }
+  return peak;
 };
