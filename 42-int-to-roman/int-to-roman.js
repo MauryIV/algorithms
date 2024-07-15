f66d2aef874edeb5bfbe04eaac0ef88cad6c656b
MauryIV <mauryhughesiv@gmail.com>
2024-07-14 20:15:12 -0600

    42-int-to-roman completed

--- 42-int-to-roman/int-to-roman.js
+++ 42-int-to-roman/int-to-roman.js
@@ -1,3 +1,43 @@
 // Write a function that takes in an integer and returns it as a Roman numeral string
 
-var intToRoman = function(num) {};
+var intToRoman = function(num) {
+  let roman = [];
+  while (num >= 1000) {
+    roman.push('M');
+    num = num - 1000;
+  } if (num >= 900) {
+    roman.push('CM');
+    num = num - 900;
+  } if (num >= 500) {
+    roman.push('D');
+    num = num - 500;
+  } if (num >= 400) {
+    roman.push('CD');
+    num = num - 400;
+  } while (num >= 100) {
+    roman.push('C');
+    num = num - 100;
+  } if (num >= 90) {
+    roman.push('XC');
+    num = num - 90;
+  } if (num >= 50) {
+    roman.push('L');
+    num = num - 50;
+  } while (num >= 10) {
+    roman.push('X');
+    num = num - 10;
+  } if (num >= 9) {
+    roman.push('IX');
+    num = num - 9;
+  } if (num >= 5) {
+    roman.push('V');
+    num = num - 5;
+  } if (num >= 4) {
+    roman.push('IV');
+    num = num - 4;
+  } while (num >= 1) {
+    roman.push('I');
+    num = num - 1;
+  }
+  return(roman.join(''))
+};
