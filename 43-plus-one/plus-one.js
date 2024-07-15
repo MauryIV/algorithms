69a9441890a4e6e4e5d5df319208fdbe4d5b2a3c
MauryIV <mauryhughesiv@gmail.com>
2024-07-15 16:06:21 -0600

    43-plus-one completed

--- 43-plus-one/plus-one.js
+++ 43-plus-one/plus-one.js
@@ -3,4 +3,20 @@
 // Solve without joining the digits array and modify the original array in place
 
 var plusOne = function(digits) {
+  if (digits.length === 0) {
+    digits.push(1);
+    return digits;
+  }
+  for (let i=digits.length-1; i>=0; i--) {
+    if (digits[i] === 9) {
+      digits[i] = 0;
+      if (i === 0) {
+        digits.unshift(1);
+        return digits;
+      }
+      continue;
+    }
+    digits[i] = digits[i]+1;
+    return digits;
+  }
 };
