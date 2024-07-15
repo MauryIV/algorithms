e33d5f2449d2684f057dc65b79bba6d56618dfd9
MauryIV <mauryhughesiv@gmail.com>
2024-07-15 16:20:06 -0600

    44-one-edit-away completed

--- 44-one-edit-away/one-edit-away.js
+++ 44-one-edit-away/one-edit-away.js
@@ -1,4 +1,14 @@
+// TODO: Write function that takes in two strings and returns true if one character away, otherwise false
 const oneEditAway = function(str1, str2) {
-  // TODO: Write function that takes in two strings and returns true if one character away, otherwise false
+  let offBy = 0;
+  for (let i=0; i<str1.length; i++) {
+    if (str1[i] !== str2[i]) {
+      offBy += 1;
+    }
   }
-  
+  if (offBy === 1) {
+    return true;
+  } else {
+    return false;
+  }
+}
