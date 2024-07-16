f8a0d5dce130a944327637f26512a8942f243dbb
MauryIV <mauryhughesiv@gmail.com>
2024-07-15 22:02:45 -0600

    48-simplify-path completed

--- 48-simplify-path/simplify-path.js
+++ 48-simplify-path/simplify-path.js
@@ -1,3 +1,15 @@
 // Write a function that takes in an absolute file path and simplifies it
 
-var simplifyPath = function(path) {};
+var simplifyPath = function(path) {
+  let thePath = [];
+  const parts = path.split('/');
+  for (let part of parts) {
+    if (part === '..') {
+      thePath.pop();
+    }
+    else if (part !== '.' && part !== '') {
+      thePath.push(part);
+    }
+  }
+  return '/' + thePath.join('/');
+};
